1f308238aa9ee2dbf69602431c0fb9831798b24c
cerXXXX <vma091416@outlook.com>
2025-02-17 20:22:54 +0300

    .

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# flutter_2
+# assets_test
 
 A new Flutter project.
 
--- android/app/src/main/kotlin/com/example/assets_test/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/assets_test/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.flutter_2
+package com.example.assets_test
 
 import io.flutter.embedding.android.FlutterActivity
 
